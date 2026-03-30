4e8fc481dca3678dc8c546beed9f895c3d1a89f4
chocodeerzynx <phylliscpx0403@proton.me>
2026-03-30 21:46:17 +0800

    Updated declaration of functions in header file

--- subvert.h
+++ subvert.h
@@ -4,7 +4,9 @@
 #include <ctype.h>
 
 
+void have_extension();
 void get_flags(int argc, char *argv[]);
+char *get_basename_with_dot(const char *input);
 void vtt_to_lrc(FILE *in, FILE *out);
 void vtt_to_srt(FILE *in, FILE *out);
 void srt_to_vtt(FILE *in, FILE *out);
